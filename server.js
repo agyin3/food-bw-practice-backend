3af5715e35789d848cdb06b3ef04b658fb3458ff
Buddy Agyin <agyin3@gmail.com>
2020-06-17 13:56:14 -0600

    Import customer auth routes

--- server.js
+++ server.js
@@ -1,8 +1,9 @@
+require('dotenv').config()
 const express = require('express')
 const helmet = require('helmet')
 const cors = require('cors')
 const BussinessUserAuth = require('./auth/business-user-auth')
-require('dotenv').config()
+const CustomerAuth = require('./auth/customer-auth')
 
 const server = express()
 server.use(express.json())
@@ -10,7 +11,7 @@ server.use(helmet())
 server.use(cors())
 
 server.use('/api/auth/bussiness-owner', BussinessUserAuth)
-
+server.use('/api/auth/customers', CustomerAuth)
 server.get('/', (req, res) => {
     res.send("IT IS ALIVE!!!!!")
 })
